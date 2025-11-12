39bc2932f36e3f5f4c7b1d772a31de06e0d1db90
Dillan73 <dilly.agrawal@gmail.com>
2025-11-12 11:34:22 -0500

    Need to fix search term part and make output scrollable

--- src/HTML_Reader25.java
+++ src/HTML_Reader25.java
@@ -1,5 +1,11 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
 import javax.swing.*;
 /*
 Links:
@@ -156,42 +162,162 @@ public class HTML_Reader25 implements ActionListener {
                 termText.setText("Please do not delete the initial text and put a valid link after the colon!");
                 return false;
             }
-            submit.setText("  Reset  ");
-            UI = false;
-            return true;
+
+            String[] allLinks = getLinks(link);
+            if(allLinks == null){
+                linkText.setText("Please input a valid link!");
+                return false;
+            }
+            if(term.equals("")){
+                setOutput(allLinks);
+
+                submit.setText("  Reset  ");
+                UI = false;
+                return true;
+            }else {
+                String[] terms = getTerms(term);
+                if (allLinks == null) {
+                    termText.setText("Please follow the formatting guidelines!");
+                    return false;
+                }
+                String[] links = constrain(allLinks, terms);
+                if (links == null) {
+                    linkText.setText("Please input a valid link!");
+                    return false;
+                }
+                setOutput(links);
+
+                submit.setText("  Reset  ");
+                UI = false;
+                return true;
+            }
         }
         submit.setText("  Press to see your links!  ");
         UI = true;
         return true;
 
+    }
+    void setOutput(String[] links){
+        String output = "";
+        System.out.println("links: " + Arrays.toString(links));
+        for(String link: links){
+            output = output + link + "\n";
+        }
+        outputText.setText(output);
+        System.out.println("output: " +output);
+    }
+    private String[] constrain(String[] allLinks, String[] terms) {
+        ArrayList<String> links= new ArrayList<>();
+        for(String link : allLinks){
+            boolean contains = true;
+            for(String term : terms){
+                if(!link.contains(term)){
+                    contains = false;
+                    break;
+                }
+            }
+            if(contains){
+                links.add(link);
+            }
+        }
+        String[] constrained = new String[links.size()];
+        for(int i = 0; i < links.size(); i++){
+            allLinks[i] = links.get(i);
+        }
+        return constrained;
+    }
 
+    private String[] getTerms(String term) {
+        return term.split("&");
+    }
+
+    String[] getLinks(String link){
+        ArrayList<String> links= new ArrayList<>();
+        try{
+            URL url = new URL(link);
+            URLConnection urlc = url.openConnection();
+            urlc.setRequestProperty("User-Agent", "Mozilla 5.0 (Windows; U; " + "Windows NT 5.1; en-US; rv:1.8.0.11) ");
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(urlc.getInputStream())
+            );
+            String line;
+            while ( (line = reader.readLine()) != null ) {
+                System.out.println(line);
+                //href(line, links);
+                src(line, links);
+            }
+
+        }catch(Exception e){
+            System.out.println("getLinks");
+            return null;
+        }
+        String[] allLinks = new String[links.size()];
+        for(int i = 0; i < links.size(); i++){
+            allLinks[i] = links.get(i);
+        }
+        return allLinks;
+    }
+    void href(String line, ArrayList<String> links){
+        String[] parts = line.split("href=");
+        for(int i = parts.length-1; i >0; i--){
+            String after = parts[i].substring(1);
+            int quoteIndex = after.indexOf("\"");
+            int apostropheIndex = after.indexOf("\'");
+            if(quoteIndex < 0){
+                quoteIndex = after.length();
+            }
+            if(apostropheIndex < 0){
+                apostropheIndex = after.length();
+            }
+            int index = Math.min(quoteIndex, apostropheIndex);
+            String link = after.substring(0,index);
+            links.add(link);
+        }
+    }
+    void src(String line, ArrayList<String> links){
+        String[] parts = line.split("src=");
+        for(int i = parts.length-1; i >0; i--){
+            String after = parts[i].substring(1);
+            int quoteIndex = after.indexOf("\"");
+            int apostropheIndex = after.indexOf("\'");
+            if(quoteIndex < 0){
+                quoteIndex = after.length();
+            }
+            if(apostropheIndex < 0){
+                apostropheIndex = after.length();
+            }
+            int index = Math.min(quoteIndex, apostropheIndex);
+            String link = after.substring(0,index);
+            links.add(link);
+        }
     }
     private void makeMenu(){
-        cut = new JMenuItem("cut");
-        copy = new JMenuItem("copy");
-        paste = new JMenuItem("paste");
-        selectAll = new JMenuItem("selectAll");
-        cut.addActionListener(this);
-        copy.addActionListener(this);
-        paste.addActionListener(this);
-        selectAll.addActionListener(this);
-
-        mb = new JMenuBar();
-        file = new JMenu("File");
-        edit = new JMenu("Edit");
-        help = new JMenu("Help");
-        edit.add(cut);
-        edit.add(copy);
-        edit.add(paste);
-        edit.add(selectAll);
-        mb.add(file);
-        mb.add(edit);
-        mb.add(help);
-        //end menu at top
-
-
-        mainFrame.add(mb);  //add menu bar
-        mainFrame.setJMenuBar(mb);
+//        cut = new JMenuItem("cut");
+//        copy = new JMenuItem("copy");
+//        paste = new JMenuItem("paste");
+//        selectAll = new JMenuItem("selectAll");
+//        cut.addActionListener(this);
+//        copy.addActionListener(this);
+//        paste.addActionListener(this);
+//        selectAll.addActionListener(this);
+//
+//        mb = new JMenuBar();
+//        file = new JMenu("File");
+//        edit = new JMenu("Edit");
+//        help = new JMenu("Help");
+//        edit.add(cut);
+//        edit.add(copy);
+//        edit.add(paste);
+//        edit.add(selectAll);
+//        mb.add(file);
+//        mb.add(edit);
+//        mb.add(help);
+//        //end menu at top
+//
+//
+//        mainFrame.add(mb);  //add menu bar
+//        mainFrame.setJMenuBar(mb);
     }
 
     private void showEventDemo() {
@@ -221,7 +347,10 @@ public class HTML_Reader25 implements ActionListener {
             String command = e.getActionCommand();
 
             if (command.equals("Submit")) {
-                sumbitUI();
+                boolean success = sumbitUI();
+                if(!success){
+                    cardLayoutMain.next(cardMain);
+                }
             }
         }
     }
